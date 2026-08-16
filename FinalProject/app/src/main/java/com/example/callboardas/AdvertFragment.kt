55da3e098b2c30976cda62abc04d7aef52fbb7ff
Alexey Kleinerman <87587342+Lamer007@users.noreply.github.com>
2026-08-16 12:42:54 +0200

    finished advert fragment

--- FinalProject/app/src/main/java/com/example/callboardas/AdvertFragment.kt
+++ FinalProject/app/src/main/java/com/example/callboardas/AdvertFragment.kt
@@ -5,60 +5,88 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.callboardas.databinding.FragmentAdvertBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AdvertFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AdvertFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentAdvertBinding? = null
+    private val binding get() = _binding!!
+    private var mListener: OnFragmentInteractionListener? = null
+    private val db: DataBase by lazy { DataBase(requireContext()) }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    interface OnFragmentInteractionListener {
+        fun theChosenCall(callId: Int)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentAdvertBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initComponents()
+    }
+
+    private fun  initComponents() {
+        val mainActivity = activity as? MainActivity
+        val currency = mainActivity?.currentUser?.currency
+
+        binding.textViewAdvertPrice.setText("Price in ${currency}")
+
+        binding.buttonCreate.setOnClickListener { view -> when (view?.id) {
+            R.id.buttonCreate -> {makeAdvert()}
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_advert, container, false)
-    }
+    fun makeAdvert() {
+        val mainActivity = activity as? MainActivity
+        val author = mainActivity?.currentUser?.name
+        val authorId = mainActivity?.currentUser?.userId
+        val phone = mainActivity?.currentUser?.phone
+        val currency = mainActivity?.currentUser?.currency
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AdvertFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AdvertFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+         val advertType = when(binding.radioGroupAdvertType.checkedRadioButtonId) {
+         binding.radioButtonCall.id -> binding.radioButtonCall.text.toString()
+         binding.radioButtonService.id -> binding.radioButtonService.text.toString()
+         else -> ""}
+
+        val title = binding.editTextAdvertTitle.text.toString()
+        val description = binding.editTextAdvertDescription.text.toString()
+        val address = binding.editTextAdvertAddress.text.toString()
+        val price = binding.editTextAdvertPrice.text.toString()
+
+        if(advertType.isEmpty() || title.isEmpty() || description.isEmpty() || address.isEmpty() || price.isEmpty()) {
+            Toast.makeText(requireContext(), "Please fill in all fields", Toast.LENGTH_SHORT).show()
+            return
+        }
+        else {
+            if(advertType == "Call") {
+                db.addCall(title, description, address, author.toString(), authorId!!.toInt(), phone!!.toLong(), price.toInt(), currency.toString())
+                Toast.makeText(requireContext(), "Call is created", Toast.LENGTH_SHORT).show()
+                clearAdvert()
+                return
             }
+
+            if(advertType == "Service") {
+                db.addService(title, description, address, author.toString(), authorId!!.toInt(), phone!!.toLong(), price.toInt(), currency.toString())
+                Toast.makeText(requireContext(), "Service is created", Toast.LENGTH_SHORT).show()
+                clearAdvert()
+                return
+            }
+        }
     }
 
-    // val advertType = when(binding.radioGroupAdvertType.checkedRadioButtonId) {
-    // binding.radioButtonCall.id -> binding.radioButtonCall.text.toString()
-    // binding.radioButtonService.id -> binding.radioButtonService.text.toString()
-    // else -> ""}
+    fun clearAdvert() {
+        binding.radioGroupAdvertType.clearCheck()
+        binding.editTextAdvertTitle.text.clear()
+        binding.editTextAdvertDescription.text.clear()
+        binding.editTextAdvertAddress.text.clear()
+        binding.editTextAdvertPrice.text.clear()
+    }
 }
